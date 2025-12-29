67348a93f7bdcca407164094a53e73f9e07c702e
trentglobal-sg <paul.chor@trentglobal.edu.sg>
2025-12-29 05:28:35 +0000

    Added FourSquare Map example

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
+
 const app = express();
+
+app.use(cors());
 
 // all variables defined in the env file will 
 // be available in the process.env variable
